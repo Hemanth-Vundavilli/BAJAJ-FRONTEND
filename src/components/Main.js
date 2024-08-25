3ab81c7b476f080bc2d8c22b3ef0cc3b16536e69
Hemanth-Vundavilli <118917390+Hemanth-Vundavilli@users.noreply.github.com>
2024-08-25 14:08:22 +0530

    Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,7 @@ const Main = () => {
       }
 
       // Make POST request to the backend API
-      const res = await axios.post('http://localhost:5000/bfhl', parsedInput);
+      const res = await axios.post('https://bajaj-backend-final.onrender.com/bfhl', parsedInput);
       setResponse(res.data);
       setFilteredData(null); // Reset filtered data
     } catch (err) {
